338598419a65a8616213aa96c1bc25969a679639
Erwin <e.hoogerwoord@gmail.com>
2015-10-29 10:19:22 +0100

    initial commit

---
+++ ControllerPi/main.py
@@ -0,0 +1,6 @@
+# Controls the rotating platform
+#
+# The servo rotates the platform through a friction wheel.
+# This wheel can slip when it is blocked in any way as a protection.
+#
+# A connected webcam looks down on the platform to control position.
---
+++ README.md
@@ -0,0 +1,3 @@
+This repository contains the software that runs on the hardware in the DigitalFoodOne setup.
+
+Each of the hardware components gets its own folder. 
