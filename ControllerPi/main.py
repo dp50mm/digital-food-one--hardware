b5d2f22cae2bbdd36d60df3f2a72e8777f9b1b0a
Erwin <e.hoogerwoord@gmail.com>
2015-11-03 11:18:34 +0100

    serial motor control and loop

--- ControllerPi/main.py
+++ ControllerPi/main.py
@@ -9,22 +9,35 @@ import requests
 
 import picamera
 
+import serial
+
+import time
+
+ser = serial.Serial('/dev/tty.usbserial',9600)
+
 camera = picamera.PiCamera()
 
 
 
 print("testing")
 
-r = requests.get('http://digitalfoodone.appspot.com/controlrequest')
-print(r.status_code)
-print(r.headers['content-type'])
-print(r.encoding)
-print(r.text)
-data = r.json()
-print(data)
-upload_url = data['upload_url']
+def turn_and_capture():
+    r = requests.get('http://digitalfoodone.appspot.com/controlrequest')
+    print(r.status_code)
+    print(r.headers['content-type'])
+    print(r.encoding)
+    print(r.text)
+    data = r.json()
+    print(data)
+    upload_url = data['upload_url']
 
-camera.capture('my_img.jpg')
+    camera.capture('my_img.jpg')
 
-files = {'file':open('my_img.jpg','rb')}
-resp = requests.post(upload_url,files=files)
+    files = {'file':open('my_img.jpg','rb')}
+    resp = requests.post(upload_url,files=files)
+    print(resp)
+    time.sleep(30)
+    ser.write('5')
+    turn_and_capture()
+
+turn_and_capture()
