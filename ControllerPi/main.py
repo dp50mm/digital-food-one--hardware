c63b25b94d74ab382d1b992b8b866629cb2e6c49
Erwin <e.hoogerwoord@gmail.com>
2015-11-03 08:42:09 +0100

    added initial request to python

--- ControllerPi/main.py
+++ ControllerPi/main.py
@@ -4,5 +4,14 @@
 # This wheel can slip when it is blocked in any way as a protection.
 #
 # A connected webcam looks down on the platform to control position.
+import datetime
+import requests
 
 print("testing")
+
+r = requests.get('controlrequest')
+r.status_code
+r.headers['content-type']
+r.encoding
+r.text
+r.json()
