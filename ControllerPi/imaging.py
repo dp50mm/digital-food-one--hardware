269337d1ef3391a749d7911642741e82baf8036a
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 10:18:28 +0100

    structure

--- ControllerPi/imaging.py
+++ ControllerPi/imaging.py
@@ -13,20 +13,11 @@ camera.awb_mode = 'fluorescent'
 
 def capture(session_name, state):
     r = requests.get('http://digitalfoodone.appspot.com/controlrequest')
-    print('turn and capture function start')
-    print(r.status_code)
-    print(r.headers['content-type'])
-    print(r.encoding)
-    print(r.text)
     data = r.json()
-    print(data)
     upload_url = data['upload_url']
 
     camera.capture('my_img.jpg')
-    print('image captured')
     time.sleep(1)
     files = {'file':open('my_img.jpg','rb')}
     resp = requests.post(upload_url, files=files, data={'session_name':session_name,'state':state})
-    headers = {'Content-Type':'application/json'}
-    print(resp)
-    return resp.json()
+    return resp
