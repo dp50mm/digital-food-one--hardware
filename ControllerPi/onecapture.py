22c6a7389b51d4b3035647d6bfcbfdc46ff498c4
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 12:01:34 +0100

    structure

--- ControllerPi/onecapture.py
+++ ControllerPi/onecapture.py
@@ -24,6 +24,7 @@ if data['state'] == 'rewind':
     print('Received http status code: '+str(resp.status_code))
     data = resp.json()
     print('Status: '+data['status'])
-    analysis_resp = analysis.red_spot('rotationtest')
+    analysis_resp = analysis.red_spot(session_name)
+    print('Received http status code: '+str(analysis_resp.status_code))
     analysis_data = analysis_resp.json()
     print('Analysis status: '+analysis_data['status'])
