1e8f0978f9c35dfe0f5d55e1a043356f7e3dae0d
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 07:03:49 +0100

    analysis

--- ControllerPi/main.py
+++ ControllerPi/main.py
@@ -7,8 +7,6 @@
 import datetime
 import requests
 
-import picamera
-
 import serial
 
 import time
@@ -17,33 +15,12 @@ import sys
 
 ser = serial.Serial(sys.argv[1],9600)
 
-camera = picamera.PiCamera()
-camera.resolution = (1900, 1200)
-camera.awb_mode = 'fluorescent'
-
-
-print("testing")
+import imaging
 
 counter = 0
 
 def turn_and_capture(counter):
-    r = requests.get('http://digitalfoodone.appspot.com/controlrequest')
-    print(r.status_code)
-    print(r.headers['content-type'])
-    print(r.encoding)
-    print(r.text)
-    data = r.json()
-    print(data)
-    upload_url = data['upload_url']
-
-    camera.capture('my_img.jpg')
-
-    files = {'file':open('my_img.jpg','rb')}
-    data = {
-        'green points':'abc',
-    }
-    resp = requests.post(upload_url,files=files, data=data)
-    print(resp)
+    imaging.capture('no session')
     ser.write(chr(5))
     counter = counter+1
     if(counter < 100):
