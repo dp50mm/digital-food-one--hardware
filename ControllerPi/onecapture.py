66d9fb209fd03d4ffb69bc39fbd1b3d82343a320
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 11:08:09 +0100

    structure

--- ControllerPi/onecapture.py
+++ ControllerPi/onecapture.py
@@ -5,6 +5,10 @@ import requests
 import imaging
 import analysis
 
+import serial
+
+ser = serial.Serial(sys.argv[1], 9600)
+
 session_name = raw_input('Input session name: ')
 print('Sending photo to session: -'+session_name+'-')
 
@@ -12,10 +16,12 @@ req = requests.get('http://digitalfoodone.appspot.com/controllersessionstatus', 
 print(str(req.status_code))
 data = req.json()
 print(data['state'])
-
-
-# resp = imaging.capture(session_name,'capturing')
-# print('Received http status code: '+str(resp.status_code))
-# data = resp.json()
-# print('Status: '+data['status'])
-#analysis.red_spot('rotationtest')
+if data['state'] == 'rewind':
+    ser.write(chr(1))
+    ser.write(chr(1))
+    ser.write(chr(1))
+    resp = imaging.capture(session_name,'capturing')
+    print('Received http status code: '+str(resp.status_code))
+    data = resp.json()
+    print('Status: '+data['status'])
+    analysis.red_spot('rotationtest')
