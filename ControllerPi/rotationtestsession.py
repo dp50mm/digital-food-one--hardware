9fce2b4c0ed5eccf2c890ee25eccc633f30fca94
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 07:04:40 +0100

    analysis

---
+++ ControllerPi/rotationtestsession.py
@@ -0,0 +1,22 @@
+
+print('starting rotation test session')
+
+session_name = raw_input('Input session name: ')
+print('starting session: '+session_name)
+
+import analysis
+import serial
+
+counter = 0
+
+ser = serial.Serial(sys.argv[1],9600)
+
+def turn_and_capture(counter):
+    imaging.capture('rotationtest')
+    analysis.red_spot('rotationtest')
+    ser.write(chr(5))
+    counter = counter+1
+    if(counter < 100):
+        turn_and_capture(counter)
+
+turn_and_capture(counter)
