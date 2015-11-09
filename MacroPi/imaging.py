0f4314da1fc2e5344a7b7d57a46a17d7cc3eeb68
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 17:27:10 +0100

    macrosession script

--- MacroPi/imaging.py
+++ MacroPi/imaging.py
@@ -11,7 +11,7 @@ camera = picamera.PiCamera()
 camera.resolution = (1900, 1200)
 camera.awb_mode = 'off'
 camera.meter_mode = 'average'
-camera.awb_gains = (1.5,1.5)
+camera.awb_gains = (1.6,1.6)
 time.sleep(1)
 
 def capture(session_name, state):
