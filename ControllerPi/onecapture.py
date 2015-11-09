608ee4111fce241125cb120ef1325a260a9411e1
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 10:40:10 +0100

    structure

--- ControllerPi/onecapture.py
+++ ControllerPi/onecapture.py
@@ -8,14 +8,12 @@ import analysis
 session_name = raw_input('Input session name: ')
 print('Sending photo to session: -'+session_name+'-')
 
-req = requests.get('http://digitalfoodone.appspot.com/controllersessionstatus', data={'session_name':session_name})
+req = requests.get('http://digitalfoodone.appspot.com/controllersessionstatus')
 print(str(req.status_code))
-# print(r.headers['content-type'])
-# print(r.encoding)
-# print(r)
 
-resp = imaging.capture(session_name,'capturing')
-print('Received http status code: '+str(resp.status_code))
-data = resp.json()
-print('Status: '+data['status'])
+
+# resp = imaging.capture(session_name,'capturing')
+# print('Received http status code: '+str(resp.status_code))
+# data = resp.json()
+# print('Status: '+data['status'])
 #analysis.red_spot('rotationtest')
