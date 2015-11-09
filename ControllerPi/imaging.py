3d303cf791b053be27b594f34bcd608173d94516
Erwin <e.hoogerwoord@gmail.com>
2015-11-09 16:48:37 +0100

    macrosession script

--- ControllerPi/imaging.py
+++ ControllerPi/imaging.py
@@ -7,16 +7,17 @@ import serial
 
 import time
 
-camera = picamera.PiCamera()
-camera.resolution = (1900, 1200)
-camera.awb_mode = 'fluorescent'
+
 
 def capture(session_name, state):
     r = requests.get('http://digitalfoodone.appspot.com/controlrequest')
     data = r.json()
     upload_url = data['upload_url']
-
+    camera = picamera.PiCamera()
+    camera.resolution = (1900, 1200)
+    camera.awb_mode = 'fluorescent'
     camera.capture('my_img.jpg')
+    camera.close()
     time.sleep(1)
     files = {'file':open('my_img.jpg','rb')}
     resp = requests.post(upload_url, files=files, data={'session_name':session_name,'state':state})
